d89821a8a52515a329b0f1ac61132295be00023f
SJ Nealond <sj.nealond@simplepractice.com>
2023-04-06 12:01:44 -0500

    final version

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Defining a baseURL and key to as part of the request URL
 
 const baseURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
-const key = '3Cm3bHxG1I3ROE2N94Y8vw7347XEAaQk';
+const key = 'KBkCj080Lwu4i6BNyZV5ZyYNClbZ5Utu';
 
 // Grab references to all the DOM elements you'll need to manipulate
 const searchTerm = document.querySelector('.search');
